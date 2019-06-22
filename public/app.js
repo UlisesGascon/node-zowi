1b2b2493038f8c1ec1ec59df91aeb1f9090c2bbe
ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-06-22 12:58:32 +0200

    Basic UI

--- public/app.js
+++ public/app.js
@@ -2,7 +2,26 @@ var socket = io()
 
 socket.on('zowi:said', function (data) {
   console.log(`[zowi:said] ${data}`)
+  addToLog(data)
 })
+
 function sendCmd (cmd) {
   socket.emit('zowi:cmd', { cmd })
 }
+
+function addToLog (msg) {
+    const timestamp = new Date().getTime()
+    consoleSlc.innerHTML += `<p>[${timestamp}] ${msg}</p>`
+}
+
+
+const consoleSlc = document.getElementById('console')
+const cmdInputSlc = document.getElementById('cmd-input');
+const cmdActionSlc = document.getElementById('cmd-action');
+
+cmdActionSlc.addEventListener("click" ,evt => {
+    const cmd = cmdInputSlc.value.trim();
+    if(cmd) {
+        sendCmd(`${cmd}  \r\n`)
+    }
+})
--- zowi.js
+++ zowi.js
@@ -1,45 +1,46 @@
 const express = require('express')
-const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
-const bluetooth = require('node-bluetooth')
-const path = require('path')
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const bluetooth = require('node-bluetooth');
+const path = require('path');
 
-// Middleware
+//Middleware
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Rutas
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html')
-})
+    res.sendFile(__dirname + '/public/index.html');
+});
 
-const device = new bluetooth.DeviceINQ()
-device.listPairedDevices(console.log)
+const device = new bluetooth.DeviceINQ();
+device.listPairedDevices(console.log);
 /*
 [ { name: 'Zowi',
     address: 'b4-9d-0b-32-0a-1e',
     services: [ { channel: 1, name: 'SPP Dev' } ] } ]
 */
-const address = 'b4-9d-0b-32-0a-1e'
-const channel = 1
+const address = 'b4-9d-0b-32-0a-1e';
+const channel = 1;
 
 bluetooth.connect(address, channel, (err, connection) => {
-  if (err) return console.error(err)
+    if(err) return console.error(err);
+    
+    server.listen(3000);
 
-  server.listen(3000)
+    io.on('connection', function (socket) {
 
-  io.on('connection', function (socket) {
-    connection.on('data', (buffer) => {
-      const data = buffer.toString()
-      socket.emit('zowi:said', data)
-      console.log('[Zowi] said:', data)
-    })
+        connection.on('data', (buffer) => {
+            const data = buffer.toString()
+            socket.emit('zowi:said', data);
+            console.log('[Zowi] said:', data);
+        });
 
-    socket.on('zowi:cmd', function (data) {
-      const { cmd } = data
-      connection.write(Buffer.from(cmd, 'utf-8'), () => {
-        console.log(`[Server] said: ${cmd}`)
-      })
-    })
-  })
-})
+        socket.on('zowi:cmd', function (data) {
+            const {cmd} = data;
+            connection.write(Buffer.from(cmd, 'utf-8'), () => {
+                console.log(`[Server] said: ${cmd}`);
+              });
+        });
+    });
+  });
