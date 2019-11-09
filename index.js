f36ebaa180015913d2f8380114f125646fc3c5b5
Ulises Gascon <ulises.gascon@guidesmiths.com>
2019-11-09 11:58:15 +0100

    Refactor WIP - bluetooth returns promises

--- index.js
+++ index.js
@@ -11,68 +11,70 @@ const current = {
 const server = initServer(gestures, current);
 const io = initIO(server);
 
-const handler = (err, connection) => {
-    if(err) return console.error(err);
+(async () => {
+    try {
+        const connection = await bluetooth.connect('Zowi');
+        server.listen(3000);
+        io.on('connection', function (socket) {
+            connection.on('data', (buffer) => {
+                const data = buffer.toString()
+                socket.emit('zowi:said', data);
     
-    server.listen(3000);
-
-    io.on('connection', function (socket) {
-        connection.on('data', (buffer) => {
-            const data = buffer.toString()
-            socket.emit('zowi:said', data);
-
-            const regexDistance = /\&D (.*\d[1-9])\%/gm
-            if(data.match(regexDistance)) {
-                current.distance = parseInt(data.split(regexDistance)[1])
-                socket.emit('zowi:distance', current.distance)
-                socket.emit('zowi:status', current)
-            }
-
-            const regexBattery = /\&B (.*\d[1-9])\%/gm
-            if(data.match(regexBattery)) {
-                current.battery = parseFloat(data.split(regexBattery)[1])
-                socket.emit('zowi:battery', current.battery)
-                socket.emit('zowi:status', current)
-            }           
-
-            console.log('[Zowi] said:', data);
-        });
-
-        socket.on('zowi:cmd', data => {
-            const {cmd} = data;
-            connection.write(Buffer.from(cmd, 'utf-8'), () => {
-                console.log(`[Server] said: ${cmd}`);
-              });
-        });
-
-        socket.on('zowi:gesture', data => {
-            const {gesture} = data
-            const currentGesture = gestures[gesture]
-            if(currentGesture) {
-                const cmd = `${currentGesture} \r\n`
-                connection.write(Buffer.from(cmd, 'utf-8'), () => {
-                    console.log(`[Server][Gesture] said: ${cmd}`);
-                });
-            }
-       
-            console.log(gesture)
-        })
-
-        setInterval(() => {
-            const cmd = `D \r\n`
-            connection.write(Buffer.from(cmd, 'utf-8'), () => {
-                console.log(`[Server][Distance]: ${cmd}`);
-            });           
-        }, 100)
-
-        setInterval(() => {
-            const cmd = `B \r\n`
-            connection.write(Buffer.from(cmd, 'utf-8'), () => {
-                console.log(`[Server][Battery]: ${cmd}`);
+                const regexDistance = /\&D (.*\d[1-9])\%/gm
+                if(data.match(regexDistance)) {
+                    current.distance = parseInt(data.split(regexDistance)[1])
+                    socket.emit('zowi:distance', current.distance)
+                    socket.emit('zowi:status', current)
+                }
+    
+                const regexBattery = /\&B (.*\d[1-9])\%/gm
+                if(data.match(regexBattery)) {
+                    current.battery = parseFloat(data.split(regexBattery)[1])
+                    socket.emit('zowi:battery', current.battery)
+                    socket.emit('zowi:status', current)
+                }           
+    
+                console.log('[Zowi] said:', data);
             });
-        }, 3000)
+    
+            socket.on('zowi:cmd', data => {
+                const {cmd} = data;
+                connection.write(Buffer.from(cmd, 'utf-8'), () => {
+                    console.log(`[Server] said: ${cmd}`);
+                    });
+            });
+    
+            socket.on('zowi:gesture', data => {
+                const {gesture} = data
+                const currentGesture = gestures[gesture]
+                if(currentGesture) {
+                    const cmd = `${currentGesture} \r\n`
+                    connection.write(Buffer.from(cmd, 'utf-8'), () => {
+                        console.log(`[Server][Gesture] said: ${cmd}`);
+                    });
+                }
+            
+                console.log(gesture)
+            })
+    
+            setInterval(() => {
+                const cmd = `D \r\n`
+                connection.write(Buffer.from(cmd, 'utf-8'), () => {
+                    console.log(`[Server][Distance]: ${cmd}`);
+                });           
+            }, 100)
+    
+            setInterval(() => {
+                const cmd = `B \r\n`
+                connection.write(Buffer.from(cmd, 'utf-8'), () => {
+                    console.log(`[Server][Battery]: ${cmd}`);
+                });
+            }, 3000)
+    
+        });
 
-    });
-  };
-
-  bluetooth.connect('Zowi', handler);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+})();
